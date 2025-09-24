cd79b3a66d008aebcd2e4f4c0317b68b2d175201
Md Nayeem Sarker Asha <dev.ab.nayeem@gmail.com>
2025-09-24 18:52:14 +0600

    update metadata

--- resources/views/frontend/layouts/_app.blade.php
+++ resources/views/frontend/layouts/_app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <title>@yield('title') - @isset($site_author) {{ $site_author }} @else Discover & Hire Local Experts Near Me @endisset </title>
+    <title>@yield('title') - @isset($site_author) {{ $site_author }} @endisset </title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="title" content="{{ $meta_title ?? '' }}">
